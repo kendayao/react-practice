470445b11371f6b7cc39f00fc13c700a8714e9ce
Kaeneth Dayao <kendayao@gmail.com>
2023-07-12 21:29:39 -0700

    added note js file and styled

---
+++ src/components/Note.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Note.css"
+
+function Note() {
+
+    return (
+        <div className = "note">
+            <h1>Title</h1>
+            <p>Content</p>
+        </div>
+    )
+
+}
+
+export default Note
