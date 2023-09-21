671e2f943f9825b20ad127bb3cc91cf4bde4d571
Kaeneth Dayao <kendayao@gmail.com>
2023-09-20 20:46:20 -0700

    added logo

--- src/routes/Navigation.js
+++ src/routes/Navigation.js
@@ -1,16 +1,17 @@
 import React, {Fragment} from 'react'
 import Outlet, {Link} from 'react-router-dom'
+import {ReactComponent as CrwnLogo } from '../assets/crown.svg'
 
 function Navigation(){
     return(
       <Fragment>
         <div className="navigation">
             <Link className='logo-container' to='/'>
-                <div>Logo</div>
+                <CrwnLogo className='logo-container' />
             </Link>
             <div className='nav-links-container'>
                 <Link className='nav-link' to='/shop'>
-                
+                  SHOP
                 </Link>
 
             </div>
