76f2f4d73b2eebba524ad795d636e2b5f2788928
Kaeneth Dayao <kendayao@gmail.com>
2023-09-20 21:13:15 -0700

    added navigationstyling

--- src/routes/Navigation.js
+++ src/routes/Navigation.js
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react'
-import Outlet, {Link} from 'react-router-dom'
+import {Outlet, Link} from 'react-router-dom'
 import {ReactComponent as CrwnLogo } from '../assets/crown.svg'
+import './Navigation.scss'
 
 function Navigation(){
     return(
