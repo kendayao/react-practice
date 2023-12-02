3ae297c939aaf4a5d192a4d7ce5f8206d4ae8017
Kaeneth Dayao <kendayao@gmail.com>
2023-12-01 22:46:40 -0800

    setup sign up form with inputs

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+
+const defaultFormField = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
+
+function SignUpForm(){
+
+    const [formFields, setFormFields] = useState(defaultFormField)
+    const {displayName, email, password, confirmPassword}=formFields
+
+    console.log(formFields)
+
+    const handleChange = (event) => {
+        const {name,value} = event.target;
+        setFormFields({...formFields, [name]:value})
+
+    }
+
+    return(
+        <div>
+            <h1>Sign up with your email and password</h1>
+            <form>
+                <label>Display Name</label>
+                <input type="text" name="displayName" value={displayName} onChange={handleChange} required/>
+
+                <label>Email</label>
+                <input type="email" name="email" value={email} onChange={handleChange} required/>
+
+                <label>Password</label>
+                <input type="password" name="password" value={password} onChange={handleChange} required/>
+
+                <label>Confirm Password</label>
+                <input type="password" name="confirmPassword" value={confirmPassword} onChange={handleChange} required/>
+
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    )
+}
+
+
+export default SignUpForm
--- src/routes/SignIn.js
+++ src/routes/SignIn.js
@@ -1,15 +1,16 @@
 import {useEffect, React} from 'react'
 import { getRedirectResult } from 'firebase/auth'
 import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from "../utils/firebase/firebase"
+import SignUpForm from '../components/SignUpForm'
 
 function SignIn(){
 
-    useEffect(async()=>{
-        const response = await getRedirectResult(auth)
-        if(response){
-            const userDocRef = await createUserDocumentFromAuth(response.user)
-        }
-    },[])
+    // useEffect(async()=>{
+    //     const response = await getRedirectResult(auth)
+    //     if(response){
+    //         const userDocRef = await createUserDocumentFromAuth(response.user)
+    //     }
+    // },[])
 
     const logGoogleUser = async () => {
         const response = await signInWithGooglePopup()
@@ -21,7 +22,8 @@ function SignIn(){
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
+            {/* <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
+            <SignUpForm/>
         </div>
     )
     
