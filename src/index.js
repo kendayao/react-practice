070a7b287299d0f4ea892920df1bebb09cdd7c0f
Kaeneth Dayao <kendayao@gmail.com>
2024-01-14 17:56:14 -0800

    setup product context

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./components/App"
 import { BrowserRouter } from 'react-router-dom'
 import "./index.scss"
 import {UserProvider} from './contexts/user.context'
+import { ProductsProvider } from './contexts/products.context';
 
 
 
@@ -12,7 +13,9 @@ const root = createRoot(container)
 root.render(
     <BrowserRouter>
         <UserProvider>
-            <App />
+            <ProductsProvider>
+                <App />
+            </ProductsProvider>
         </UserProvider>
     </BrowserRouter>
 )
