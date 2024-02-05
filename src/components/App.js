83a3e0638c92af7e239c5a9aa346dee03e74fa3f
Kaeneth Dayao <kendayao@gmail.com>
2024-02-04 16:12:08 -0800

    created checkoutpage and increment feature

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Home from "../routes/Home"
 import Navigation from '../routes/Navigation'
 import Authentication from '../routes/Authentication'
 import Shop from '../routes/Shop'
+import Checkout from '../routes/Checkout'
 
 
 function App(){
@@ -15,6 +16,7 @@ function App(){
           <Route index element={<Home/>}/>
           <Route path='shop' element={<Shop/>}/>
           <Route path='auth' element={<Authentication/>}/>
+          <Route path='checkout' element={<Checkout/>}/>
         </Route>
       
 
--- src/components/CartDropdown.js
+++ src/components/CartDropdown.js
@@ -1,4 +1,5 @@
 import React, {useContext} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { CartContext } from '../contexts/cart.context'
 import Button from './Button'
@@ -9,13 +10,17 @@ import './CartDropdown.scss'
 function CartDropdown(){
 
     const {cartItems} = useContext(CartContext)
+    const navigate = useNavigate();
+    const goToCheckoutHandler = () => {
+        navigate('/checkout')
+    }
 
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
                 {cartItems.map(item=><CartItem key={item.id} cartItem={item}/>)}
             </div>
-            <Button>CHECKOUT</Button>
+            <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
         </div>
     )
 
