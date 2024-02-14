7cb63598a5b451d857bd9730760f1b1f2fa47f95
Kaeneth Dayao <kendayao@gmail.com>
2024-02-13 20:29:40 -0800

    added checkout component styles

--- src/routes/Checkout.js
+++ src/routes/Checkout.js
@@ -7,9 +7,25 @@ function Checkout(){
 
     const {cartItems, addItemToCart, removeItemFromCart}=useContext(CartContext)
     return(
-        <div>
-            <h1>I am the checkout page</h1>
-            <div>
+        <div className='checkout-container'>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div className='header-block'>
+                    <span>Description</span>
+                </div>
+                <div className='header-block'>
+                    <span>Quantity</span>
+                </div>
+                <div className='header-block'>
+                    <span>Price</span>
+                </div>
+                <div className='header-block'>
+                    <span>Remove</span>
+                </div>
+            </div>
+          
                 {cartItems.map((cartItem)=>{
                     const { id, name, quantity} = cartItem;
                     return (
@@ -23,7 +39,7 @@ function Checkout(){
                         </div>
                     )
                 })}
-            </div>
+            <span className='total'>Total: 0</span>
         </div>
     )
 }
