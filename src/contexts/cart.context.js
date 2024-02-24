3502b783bd3c885d8ca01a4e26e4ce0d3d262d9c
Kaeneth Dayao <kendayao@gmail.com>
2024-02-23 17:52:20 -0800

    added total cart function

--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -48,7 +48,8 @@ export const CartContext = createContext({
     addItemToCart: ()=>{},
     removeItemFromCart: ()=>{},
     clearItemFromCart: ()=>{},
-    cartCount: 0
+    cartCount: 0,
+    cartTotal: 0
 })
 
 
@@ -56,10 +57,16 @@ export const CartProvider = ({children})=>{
     const [isCartOpen, setIsCartOpen]=useState(false)
     const [cartItems, setCartItems]=useState([])
     const [cartCount, setCartCount]=useState(0)
+    const [cartTotal, setCartTotal]=useState(0)
 
     useEffect(()=>{
         const newCartCount = cartItems.reduce((total, cartItem)=> total + cartItem.quantity, 0)
         setCartCount(newCartCount)
+    },[cartItems])
+
+    useEffect(()=>{
+        const newCartTotal = cartItems.reduce((total, cartItem)=>total+cartItem.quantity*cartItem.price, 0)
+        setCartTotal(newCartTotal)
     },[cartItems])
 
 
@@ -75,7 +82,7 @@ export const CartProvider = ({children})=>{
         setCartItems(clearCartItem(cartItems, cartItemToClear))
     }
 
-    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount, removeItemFromCart, clearItemFromCart}
+    const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount, removeItemFromCart, clearItemFromCart, cartTotal}
     return(
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
     )
