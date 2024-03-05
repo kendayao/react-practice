9f7ae23aab31ecfb50c95b2c8b84e515f80fb1b6
Kaeneth Dayao <kendayao@gmail.com>
2024-03-04 20:58:00 -0800

    created categories preview page and set up routes through shop component

--- src/routes/Shop.js
+++ src/routes/Shop.js
@@ -1,23 +1,13 @@
-import {React, useContext, Fragment} from 'react'
-import { CategoriesContext } from '../contexts/categories.context'
-
-import CategoryPreview from '../components/CategoryPreview'
+import {Routes, Route} from 'react-router-dom'
 import './Shop.scss'
-
+import CategoriesPreview from './CategoriesPreview'
 
 function Shop () {
-    const {categoriesMap} = useContext(CategoriesContext)
-    // const CATEGORIES_KEYS = ['jackets', 'hats', 'mens', 'sneakers', 'womens']
-
+  
     return(
-        <div className='shop-container'>
-             {Object.keys(categoriesMap).map((title)=>{
-                const products = categoriesMap[title]
-                console.log(products)
-                return <CategoryPreview key={title} title={title} products={products} />
-                
-                })}
-        </div>                   
+        <Routes>
+            <Route index element={<CategoriesPreview/>}/>
+        </Routes>       
     )
     }
 
